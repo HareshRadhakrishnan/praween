6c96fa3000d5d3c93077b0f58ee9ee7f07fa0664
HareshRadhakrishnan <haresh2569@gmail.com>
2024-09-18 20:11:27 +0530

    initail changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,46 +2,51 @@ import { Button } from "./ui/button";
 import { buttonVariants } from "./ui/button";
 import { HeroCards } from "./HeroCards";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import HeroBG from "../assets/hero-bg.png";
 
 export const Hero = () => {
   return (
-    <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
-      <div className="text-center lg:text-start space-y-6">
-        <main className="text-5xl md:text-6xl font-bold">
-          <h1 className="inline">
-            <span className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
-              Shadcn
-            </span>{" "}
-            landing page
-          </h1>{" "}
-          for{" "}
-          <h2 className="inline">
-            <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
-              React
-            </span>{" "}
-            developers
-          </h2>
-        </main>
-
-        <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
-          Build your React landing page effortlessly with the required sections
-          to your project.
-        </p>
-
-        <div className="space-y-4 md:space-y-0 md:space-x-4">
-          <Button className="w-full md:w-1/3">Get Started</Button>
-
-          <a
-            rel="noreferrer noopener"
-            href="https://github.com/leoMirandaa/shadcn-landing-page.git"
-            target="_blank"
-            className={`w-full md:w-1/3 ${buttonVariants({
-              variant: "outline",
-            })}`}
-          >
-            Github Repository
-            <GitHubLogoIcon className="ml-2 w-5 h-5" />
-          </a>
+    <section className="">
+      <div className="bg-white dark:bg-gray-800 flex relative z-20 items-center h-screen overflow-hidden">
+        <div className="absolute inset-0">
+          <img
+            src={HeroBG}
+            alt="Background Image"
+            className="object-cover object-center w-full h-full"
+          />
+          <div className="absolute inset-0 bg-black opacity-50"></div>
+        </div>
+        <div className="container text-white mx-auto px-6 flex relative py-16">
+          <div className="sm:w-2/3 lg:w-2/5 flex flex-col relative z-20">
+            <h1 className="text-2xl font-bold mb-4 text-gray-300">
+              Experience Sri Lanka:{" "}
+            </h1>
+            <h1 className="text-5xl font-bold mb-4">
+              Your Gateway to Unforgettable Adventures in 2024
+            </h1>
+            <p className="text-sm sm:text-base dark:text-gray-700 text-white">
+              Welcome to the ultimate travel destination of 2024! Discover Sri
+              Lanka's enchanting beauty and vibrant culture. From pristine
+              beaches to lush forests, ancient temples to bustling markets,
+              explore every hidden gem with our guide. Whether you seek
+              thrilling adventures or serene escapes, Sri Lanka offers an
+              unforgettable journey. Start planning your dream vacation today!
+            </p>
+            <div className="flex mt-8 gap-2">
+              <a
+                href="#"
+                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+              >
+                Get started
+              </a>
+              <a
+                href="#"
+                className="bg-transparent hover:bg-gray-800 text-white font-bold py-2 px-4 rounded border-2 border-white"
+              >
+                Read more
+              </a>
+            </div>
+          </div>
         </div>
       </div>
 
